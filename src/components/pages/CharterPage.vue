815af0ff944fb8b02715cbcc8bb3fb1675c71f82
Thibault Clérice <thibault.clerice@inria.fr>
2026-05-30 20:51:28 +0200

    Fix charter URL; render with GFM; add project filter; trim project/license whitespace

--- src/components/pages/CharterPage.vue
+++ src/components/pages/CharterPage.vue
@@ -24,28 +24,24 @@ import { ref, onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
 import TopBar from '../TopBar.vue'
 import AppFooter from '../AppFooter.vue'
+import { renderGfm } from '../../utils/markdown.js'
 
 const { t } = useI18n()
 const loading = ref(true)
 const error   = ref(null)
 const html    = ref('')
 
-const CHARTER_URL = 'https://raw.githubusercontent.com/HTR-United/htr-united/master/data-reuse-charter.md'
+const CHARTER_URL = 'https://raw.githubusercontent.com/HTR-United/htr-united/master/reuse-charter.md'
 
 onMounted(async () => {
   try {
     const res = await fetch(CHARTER_URL)
     if (!res.ok) throw new Error(`HTTP ${res.status}`)
-    const text = await res.text()
-    html.value = '<pre style="white-space:pre-wrap;font-size:14px;line-height:1.7">' + esc(text) + '</pre>'
+    html.value = renderGfm(await res.text())
   } catch (e) {
     error.value = e.message
   } finally {
     loading.value = false
   }
 })
-
-function esc(s) {
-  return s.replace(/[&<>"]/g, c => ({ '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;' }[c]))
-}
 </script>
